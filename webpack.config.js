0b5bd23df5128b3321e71ff4f246dc25647a2fdb
Arnaud <bandonkeyea@gmail.com>
2022-05-24 10:15:11 -0600

    Display items fetch from api #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
- const path = require('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     },
-     devtool: 'inline-source-map',
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
@@ -14,15 +15,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-       path: path.resolve(__dirname, 'dist'),
-     clean: true,
-     },
-   optimization: {
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
     runtimeChunk: 'single',
   },
-   module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
@@ -30,4 +31,4 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       },
     ],
   },
- };
+};
